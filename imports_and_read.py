a53d19256ccffd2bb71a50a6418e52948bd7a5b1
Rishab Koul <rishabkoul2001@gmail.com>
2021-02-10 16:06:59 +0530

    iniitial commit

---
+++ imports_and_read.py
@@ -0,0 +1,17 @@
+#Import the libraries
+#import math
+#import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM
+# import matplotlib.pyplot as plt
+# plt.style.use('fivethirtyeight')
+from tensorflow.keras.models import load_model
+from nsepy import get_history
+from datetime import date
+import datetime
+import streamlit as st
+
+from PIL import Image
