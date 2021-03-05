d43a5f6b17051dcf596fe520d60a0979728a5f3e
Rishab Koul <rishabkoul2001@gmail.com>
2021-03-05 15:54:56 +0530

    deploy

--- stock.py
+++ stock.py
@@ -1,4 +1,22 @@
-from imports_and_read import * 
+#Import the libraries
+import os
+import math
+from dateutil.relativedelta import relativedelta
+#import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+from tensorflow.keras.models import load_model
+from nsepy import get_history
+from datetime import date
+import datetime
+import streamlit as st
+
+from PIL import Image 
 import tensorflow.keras.backend as tb
 #tb._SYMBOLIC_SCOPE.value = True
 
